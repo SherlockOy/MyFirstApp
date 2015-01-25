ba879bef92ead22e302c960bf2a518da7b766697
SherlockOy <oyang716@qq.com>
2015-01-25 16:50:49 +0800

    add responding to navigation drawer

--- app/src/main/java/com/mycompany/myfirstapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myfirstapp/MainActivity.java
@@ -2,15 +2,17 @@ package com.mycompany.myfirstapp;
 
 import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import com.mycompany.myfirstapp.connection.Connection;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -25,17 +27,45 @@ public class MainActivity extends ActionBarActivity {
     private String[] mDrawerItems;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mDrawerItems = getResources().getStringArray(R.array.drawer_item);
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
+        // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mDrawerItems));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
+
+
+    // handle navigation click events
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        // to turn to each corresponding activity
+        switch (position){
+            case 0:
+                Intent intent_1 = new Intent(MainActivity.this, Connection.class);
+                startActivity(intent_1);
+                break;
+            default:
+        }
+    }
+
+
+
 
     public void sendMessage(View view) {
 
@@ -80,3 +110,5 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
