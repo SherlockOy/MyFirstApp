893f7f8885a82e0ad671f99b549ffd26c9e97d61
SherlockOy <oyang716@qq.com>
2015-01-27 16:19:03 +0800

    let the icon roll

--- app/src/main/java/com/mycompany/myfirstapp/Welcome.java
+++ app/src/main/java/com/mycompany/myfirstapp/Welcome.java
@@ -1,9 +1,11 @@
 package com.mycompany.myfirstapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,15 +22,48 @@ public class Welcome extends ActionBarActivity {
     private String[] mDrawerItems;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mTitle;
+    private CharSequence mDrawerTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        mTitle = mDrawerTitle = getTitle();
         mDrawerItems = getResources().getStringArray(R.array.drawer_item);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                // in android studio use getSupportActionBar() instead of getActionBar()
+                // the ActionBarActivity is under the v7 support
+                getSupportActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mDrawerTitle);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, mDrawerItems));
@@ -36,6 +71,18 @@ public class Welcome extends ActionBarActivity {
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     // handle navigation click events
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
@@ -45,16 +92,18 @@ public class Welcome extends ActionBarActivity {
         }
     }
 
-    /** Swaps fragments in the main content view */
+    /**
+     * Swaps activities in the main content view
+     */
     private void selectItem(int position) {
         // to turn to each corresponding activity
-        switch (position){
+        switch (position) {
             case 0:
                 Intent intent_0 = new Intent(Welcome.this, Connection.class);
                 startActivity(intent_0);
                 break;
             case 1:
-                Intent intent_1 =  new Intent(Welcome.this, SendMessageActivity.class);
+                Intent intent_1 = new Intent(Welcome.this, SendMessageActivity.class);
                 startActivity(intent_1);
             default:
         }
@@ -78,6 +127,11 @@ public class Welcome extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+
+        // Handle your other action bar items...
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;}
 
         return super.onOptionsItemSelected(item);
     }
